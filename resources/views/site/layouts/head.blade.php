d0f6546e3e3a87ac77739fd2aeb450f8cedec3eb
najims2016 <naji58809@gmail.com>
2024-07-31 14:56:18 +0200

    Update head.blade.php

--- resources/views/site/layouts/head.blade.php
+++ resources/views/site/layouts/head.blade.php
@@ -15,6 +15,7 @@
   <!-- Font Awesome for icons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
   <!-- Custom styles -->
+  @yield('cssbefor')
   @if ($defultlang->code=='ar')
   <link rel="stylesheet" href="{{ url('assets/site/css/styles.css') }}" />
   @else
